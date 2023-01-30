1bcbbb24d537da85cb0dd141b64bd48a06bb5ab1
Suyog Adhikari <suyogadhiakri@gmail.com>
2023-01-30 11:17:28 +0545

    commit

--- admin/newsmagazine/headerFooter/footer.php
+++ admin/newsmagazine/headerFooter/footer.php
@@ -15,8 +15,18 @@
 <!-- Custom Theme JavaScript -->
 <script src="../dist/js/sb-admin-2.js"></script>
 
+<!-- DataTables JavaScript -->
+<script src="../vendor/datatables/js/jquery.dataTables.min.js"></script>
+<script src="../vendor/datatables-plugins/dataTables.bootstrap.min.js"></script>
+<script src="../vendor/datatables-responsive/dataTables.responsive.js"></script>
+
 <script>
   $(document).ready(function(){
+    
+    $('#dataTables-example').DataTable({
+            responsive: true
+    });
+
     $('#loginForm').validate();
   })
 </script>
--- admin/newsmagazine/listCategory.php
+++ admin/newsmagazine/listCategory.php
@@ -1,6 +1,12 @@
 <?php
 
     include('headerFooter/header.php');
+
+    include('../class/category.class.php');
+    $categoryObject = new Category();
+
+    $dataList = $categoryObject->retrieve();
+
     include('sidebar.php');
 
 ?>
@@ -15,6 +21,31 @@
                 <!-- /.col-lg-12 -->
             </div>
             <!-- /.row -->
+
+            <div class="row">
+            <div class="panel-body">
+                            <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>S.N.</th>
+                                        <th>Name</th>
+                                        <th>Rank</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="odd gradeX">
+                                        <td>Trident</td>
+                                        <td>Internet Explorer 4.0</td>
+                                        <td>Win 95+</td>
+                                        <td class="center">4</td>
+                                        <td class="center">X</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+            </div>
            
         </div>
         <!-- /#page-wrapper -->
